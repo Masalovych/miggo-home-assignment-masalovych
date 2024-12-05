545300afb11aa8e5ace0773d8f13bc9b145f2349
dmasalovych <dmasalovych@gmail.com>
2024-12-05 16:55:04 +0100

    ISS is moving

---
+++ README.md
@@ -0,0 +1,15 @@
+# Frontend Developer Technical Home Assignment
+
+
+## Install dependences
+Run `npm install` in the `ui` and `api` folders.
+
+
+## Run API
+
+`cd api && npx ts-node src/index.ts`
+
+## Run UI
+
+`cd ui && npm run dev`
+
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './App.css'
 import { LatLngExpression } from 'leaflet'
 import Axios from 'axios'
-// import Map from './components/Map'
+// import 'leaflet/dist/leaflet.css';
 
 import {
   useQuery,
@@ -16,6 +15,7 @@ function App() {
       Axios.get('iss', {
         signal,
       }),
+    refetchInterval: 1000,
   })
 
   if (isPending) return 'Loading...';
@@ -30,55 +30,20 @@ function App() {
   // const position: LatLngExpression = [51.505, -0.09]
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+
+    <MapContainer center={position} zoom={5} scrollWheelZoom={false} style={{ height: 536 }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+
   )
 }
 
 export default App
-
-
-
-
-// const queryClient = new QueryClient()
-
-// export default function App() {
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <Map />
-//     </QueryClientProvider>
-//   )
-// }
-
-// function Example() {
-//   const { isPending, error, data } = useQuery({
-//     queryKey: ['repoData'],
-//     queryFn: () =>
-//       fetch('https://api.github.com/repos/TanStack/query').then((res) =>
-//         res.json(),
-//       ),
-//   })
-
-//   if (isPending) return 'Loading...'
-
-//   if (error) return 'An error has occurred: ' + error.message
-
-//   return (
-//     <div>
-//       <h1>{data.name}</h1>
-//       <p>{data.description}</p>
-//       <strong>👀 {data.subscribers_count}</strong>{' '}
-//       <strong>✨ {data.stargazers_count}</strong>{' '}
-//       <strong>🍴 {data.forks_count}</strong>
-//     </div>
-//   )
-// }
